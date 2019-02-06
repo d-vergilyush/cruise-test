f45c9d823c50808a50932e4eb233e42d18b8e604
Dana Vergilyush <dvergilyush@gmail.com>
2019-02-06 14:31:23 +0300

    clean

--- pages/common/menu/menu.js
+++ pages/common/menu/menu.js
@@ -1,20 +1,3 @@
-// $(document).ready(function () {
-//     $(".menu__item, [data-scroll]").on("click", function (e) {
-//         e.preventDefault();
-
-//         var id = $(this).attr('href') || $(this).attr('data-scroll');
-//         if (id) {
-//             var top = $(id).offset().top;
-//             $('body,html').animate({scrollTop: top}, 1500);
-//         }
-
-//         var target = $(this).attr('data-target');
-//         if (target) {
-//             yaCounter51455630.reachGoal(`${target}`);
-//         }
-//     });
-// });
-
 window.addEventListener('DOMContentLoaded', function () {
 	const burger = document.querySelector('.jsShowMenu');
 	const menu = document.querySelector('.jsMainMenu');
